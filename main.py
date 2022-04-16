9d1fb91962b93e8bead44feaf9bc819ffaba6757
alexxpeng <apeng@gatech.edu>
2022-04-16 18:29:52 -0400

    updated keybindings and oled

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Starting")
+print("BicOwo Starting...")
 
 import board
 
@@ -6,6 +6,26 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.matrix import DiodeOrientation
 from kmk.modules.layers import Layers
+
+import busio as io
+import adafruit_ssd1306                                                                     
+from digitalio import DigitalInOut
+import adafruit_framebuf
+
+i2c = io.I2C(board.GP17, board.GP16)#set to sda and scl
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+reset_pin = DigitalInOut(board.GP15) # any pin!
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+TH = bytearray()
+fb = adafruit_framebuf.FrameBuffer(TH, 64, 32)
+
+print("Syncing OLED Display...")
+#OLED STUFF
+oled.fill(0)
+oled.rect(0, 0, 128, 32, 1)
+
+oled.show()
+#END OF OLED STUFF
 
 
 keyboard = KMKKeyboard()
@@ -25,22 +45,24 @@ keyboard.keymap = [
     [  #NumKeys
         KC.ESC, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.DELETE,\
         KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,\
-        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,\
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
+        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.RSFT, KC.NO,\
+        KC.TRNS, KC.LALT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
     ],
     [  #Function Keys
         KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12,\
         KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,\
-        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,\
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
+        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.RSFT, KC.NO,\
+        KC.TRNS, KC.LALT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
     ],
     [  #Extras
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.MINUS, KC.EQUAL, KC.LBRACKET, KC.RBRACKET, KC.BSLASH,\
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.SCOLON, KC.QUOTE, KC.NO,\
-        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.COMMA, KC.DOT, KC.NO,\
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
+        KC.NO, KC.NO, KC.UP, KC.NO, KC.NO, KC.NO, KC.NO, KC.MINUS, KC.EQUAL, KC.LBRACKET, KC.RBRACKET, KC.BSLASH,\
+        KC.NO, KC.LEFT, KC.DOWN, KC.RIGHT, KC.NO, KC.NO, KC.NO, KC.NO, KC.SCOLON, KC.QUOTE, KC.NO, KC.NO,\
+        KC.LSFT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.COMMA, KC.DOT, KC.RSFT, KC.NO,\
+        KC.TRNS, KC.LALT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO
     ],
 ]
-
+print("Keymap Configured...")
+print("Startup Successful!")
+print("=====================")
 if __name__ == '__main__':
     keyboard.go()
